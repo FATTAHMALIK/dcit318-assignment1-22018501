24cf3510daf83fa44aa16e58108b39bebfc19046
Malik Fattah <fmalik@st.ug.edu.gh>
2026-08-26 15:34:04 -0700

    Add Ticket Price Calculator

---
+++ TicketPriceCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Enter your age: ");
+
+if (int.TryParse(Console.ReadLine(), out int age))
+{
+    if (age < 0)
+    {
+        Console.WriteLine("Invalid age.");
+    }
+    else if (age <= 12 || age >= 65)
+    {
+        Console.WriteLine("Ticket Price: GHC7");
+    }
+    else
+    {
+        Console.WriteLine("Ticket Price: GHC10");
+    }
+}
+else
+{
+    Console.WriteLine("Invalid input. Please enter a valid age.");
+}
